77f113bd3744d67273a24fe5ba3ad6b75f27cb3c
Alexander <alviurlex@gmail.com>
2018-01-09 14:51:30 +0100

    I2C pooling ready

---
+++ I2C_2/Objects/system_stm32l4xx_1.d
@@ -0,0 +1,10 @@
+.\objects\system_stm32l4xx_1.o: RTE\Device\STM32L476RGTx\system_stm32l4xx.c
+.\objects\system_stm32l4xx_1.o: D:\Keil\STM32L4xx_DFP\2.0.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\stm32l4xx.h
+.\objects\system_stm32l4xx_1.o: D:\Keil\STM32L4xx_DFP\2.0.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\stm32l476xx.h
+.\objects\system_stm32l4xx_1.o: D:\ARM\CMSIS\5.2.0\CMSIS\Include\core_cm4.h
+.\objects\system_stm32l4xx_1.o: D:\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\system_stm32l4xx_1.o: D:\ARM\CMSIS\5.2.0\CMSIS\Include\cmsis_version.h
+.\objects\system_stm32l4xx_1.o: D:\ARM\CMSIS\5.2.0\CMSIS\Include\cmsis_compiler.h
+.\objects\system_stm32l4xx_1.o: D:\ARM\CMSIS\5.2.0\CMSIS\Include\cmsis_armcc.h
+.\objects\system_stm32l4xx_1.o: D:\ARM\CMSIS\5.2.0\CMSIS\Include\mpu_armv7.h
+.\objects\system_stm32l4xx_1.o: D:\Keil\STM32L4xx_DFP\2.0.0\Drivers\CMSIS\Device\ST\STM32L4xx\Include\system_stm32l4xx.h
